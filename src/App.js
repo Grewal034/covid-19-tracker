8e9168014c172c10eff1671acf6af9c16f809370
Ricky Grewal <Rickygrewal@protonmail.com>
2022-03-15 18:47:46 -0600

    Unable to load page | Debugging one Bug

--- src/App.js
+++ src/App.js
@@ -5,15 +5,33 @@ import React from "react";
 // import Chart from "./components/Chart";
 
 import {Cards, Chart, CountryPicker} from "./components";
-
+import {fetchData} from "./api/index.js";
 import styles from "./App.module.css";
+
+import image from './images/image.png';
+
 class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
+
+
+  async componentDidMount() {
+  const data = await fetchData();
+  this.setState({ data });
+  console.log(data);
+}
+
   render() {
+    const { data, country } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
-        <Chart />
-        <CountryPicker />
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
       </div>
     );
   }
